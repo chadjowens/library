76c2c2d22768077dd4247c4df92e3d3fec877ef8
chadjowens <chadjowens@gmail.com>
2024-02-15 10:29:07 -0600

    Modified Object Constructor to return data elements passed into the info() function.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,42 @@
 
 // console.log("hello");
+////////////////////////
+// Player Object Constructor 
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
+//   this.sayName = function() {
+//     console.log(name);
+//   }
+// }
+// const player1 = new Player('Chad', 'X');
+// const player2 = new Player('Joe', 'O');
+// player1.sayName();
+// player2.sayName();
 
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-  this.sayName = function() {
-    console.log(name);
-  }
-}
-
-const player1 = new Player('Chad', 'X');
-const player2 = new Player('Joe', 'O');
-// console.log(player.name, player.marker);
-player1.sayName();
-player2.sayName();
-
-
+///////////////////////
+// Book Object Constructor 
 // function Book(title, author, pages, read) {
 //   this.title = title;
 //   this.author = author;
 //   this.pages = pages;
 //   this.read = read;
-//   theHobbit.info = function() {
-    // console.log(theHobbit.info());
-    // console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`);
-//   }
+//     this.info = function() {
+//         console.log(title, author, pages, read);    
+//     } 
 // }
-
 // const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 354, true);
-
 // theHobbit.info();
-// console.log(theHobbit.info());
+///////////////////////
+// Book Object Constructor -- returning the value of the function
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+      this.info = function() {
+           return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+      } 
+  }
+  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 354, true);
+  console.log(theHobbit.info());
